866b1b0fc6cdfeedcf32557349dd3c7ab7076604
Chloe-O <chloeosbourne@yahoo.co.uk>
2024-07-10 19:33:34 +0100

    fetching questions, 1st question displays in html

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 // questions courtesy of >>> https://gist.github.com/cmota/f7919cd962a061126effb2d7118bec72
 
-fetch("/questions.json")
-  .then((response) => response.json())
-  .then((json) => console.log(json[0].question));
+async function fetchQuestions() {
+  const response = await fetch("/questions.json");
+  const JSONquestions = await response.json();
+  return JSONquestions;
+}
 
-let startQuizBtn = document.getElementById("start-quiz-btn");
+const startQuizBtn = document.getElementById("start-quiz-btn");
+const answerElement = document.querySelectorAll(".answer");
+const a_answer = document.getElementById("a_answer");
+const b_answer = document.getElementById("b_answer");
+const c_answer = document.getElementById("c_answer");
+const d_answer = document.getElementById("d_answer");
+const HTMLquestion = document.getElementById("question");
 
-startQuizBtn.addEventListener("click", () => {
-  console.log("clicked!");
+function startQuiz() {
+  //clear quiz data f() here//
 
-  document.getElementById("start-screen").style.display = "none";
-  document.getElementById("questions-screen").style.display = "block";
-});
+  fetchQuestions().then((JSONquestion) => {
+    HTMLquestion.innerText = JSONquestion[0].question;
+  });
+
+  startQuizBtn.addEventListener("click", () => {
+    document.getElementById("start-screen").style.display = "none";
+    document.getElementById("questions-screen").style.display = "block";
+
+    // question.innerHTML =
+  });
+}
+
+startQuiz();
+
+/*
+press start begins quiz
+clear previous quiz data
+begin iterating over each question/answers to populate quiz wrapper
+if answer is correct, add one point and populate next question/answers
+if answer is incorrect, do not add one point, populate next question/answers
+get to end of quiz, show score
+clear data
+restart
+
+
+stretch > score counter
+*/
